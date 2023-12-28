f6d0a17091c2589654376d44a8b73b6ab3a74d0b
flavia sigoli <flaviasivm@gmail.com>
2023-12-28 20:06:51 -0300

    initial comit-mobile

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+//navlinks menu mobile
+let menu_icon_box = document.querySelector(".btn-toggle");
+let box = document.querySelector(".box");
+
+
+menu_icon_box.onclick = function(){
+    menu_icon_box.classList.toggle("active");
+    box.classList.toggle("active_box");
+}
+document.onclick = function(e){
+    if (!menu_icon_box.contains(e.target) && !box.contains(e.target) ) {
+        menu_icon_box.classList.remove("active");
+        box.classList.remove("active_box");
+    }
+}
